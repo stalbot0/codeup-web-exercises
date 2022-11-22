dedcf7b65e82f7ddc90dc0d554c91ee456cba8de
Stephen Talbot <stephen.clark.talbot@gmail.com>
2022-11-22 09:51:17 -0600

    implement script in  HTML

---
+++ js/external.js
@@ -0,0 +1,16 @@
+// JavaScript with HTML exercises - Codeup
+
+"use strict";
+console.log("Hello from external JavaScript");
+
+// below is the solution to number 1
+alert("Welcome to my Website!"); // this is ALSO how you irritate your users
+
+
+// below is the solution to number 2
+let newPrompt = prompt('What is your favorite color?', '');
+alert(`Great, ${newPrompt} is my favorite color too!`);
+confirm(`Are you sure that ${newPrompt} is your favorite?`); // Threw this one in for practice
+
+
+// below is the solution to number 3;
