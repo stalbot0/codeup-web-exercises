36d872ca8321c534a2b93ff28a243507ce673eee
Stephen Talbot <stephen.clark.talbot@gmail.com>
2022-12-02 15:30:14 -0600

    completed planets-string js

--- js/planets-string.js
+++ js/planets-string.js
@@ -2,7 +2,6 @@
     "use strict";
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
 
     /**
      * TODO:
@@ -10,13 +9,23 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
+    let planetsArray = planetsString.split("|");
+    console.log(planetsArray);
+    console.log(typeof planetsArray); // logs object because an array is an object
 
-    // console.log(planetsArray);
+    console.log('\n');
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
+     */
+    let str = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
+    console.log(str);
+
+    document.body.innerHTML += str;
+
+    /**
      *
      * BONUS:
      * Create another string that would display your planets in an undordered
@@ -24,4 +33,7 @@
      * string, and <li> tags around each planet.
      */
 
+    let strUnordered = "<ul><li>Mercury</li><li>Venus</li><li>Earth</li><li>Mars</li><li>Jupiter</li><li>Saturn</li><li>Uranus</li><li>Neptune</li></ul>";
+    console.log(strUnordered);
+    document.body.innerHTML += strUnordered;
 })();
