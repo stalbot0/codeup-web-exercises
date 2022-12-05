6cc43f382fae525f5157127b81e10727e42ad487
Stephen Talbot <stephen.clark.talbot@gmail.com>
2022-12-05 13:27:16 -0600

    finalized arrays exercise

--- js/planets-string.js
+++ js/planets-string.js
@@ -20,20 +20,23 @@
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
      */
-    let str = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
-    console.log(str);
+    // let str = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
+    // console.log(str);
 
-    document.body.innerHTML += str;
+    let planetsStringTwo = planetsArray.join("<br>");
+    console.log(planetsStringTwo);
+
+    document.write(planetsStringTwo);
 
     /**
      *
      * BONUS:
-     * Create another string that would display your planets in an undordered
+     * Create another string that would display your planets in an unordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-
     let strUnordered = "<ul><li>Mercury</li><li>Venus</li><li>Earth</li><li>Mars</li><li>Jupiter</li><li>Saturn</li><li>Uranus</li><li>Neptune</li></ul>";
     console.log(strUnordered);
-    document.body.innerHTML += strUnordered;
+    document.write(strUnordered);
+
 })();
