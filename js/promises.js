7e1e592075914fb0d4618968cc7eef13ca7da461
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-01-30 12:13:37 -0600

    merged to main

--- js/promises.js
+++ js/promises.js
@@ -5,7 +5,7 @@
     function getLatestCommit() {
         return fetch('https://api.github.com/users/stalbot0/events', {headers: {'Authorization': 'GITHUB_API_KEY2'}})
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => console.log(data[0]))
             .catch(error => console.error(error))
     }
 
