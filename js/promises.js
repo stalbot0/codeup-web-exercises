75c429bf0384c39ab9b0f546b0ec393cb6f73c1f
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-01-30 14:32:36 -0600

    finished promises exercise

--- js/promises.js
+++ js/promises.js
@@ -2,13 +2,32 @@
 
 (function () {
 
+    var githubUsername = "stalbot0"
+
     function getLatestCommit() {
-        return fetch('https://api.github.com/users/stalbot0/events', {headers: {'Authorization': 'GITHUB_API_KEY2'}})
+        return fetch('https://api.github.com/users/' + githubUsername + '/events', {headers: {'Authorization': 'GITHUB_API_KEY2'}})
             .then(response => response.json())
             .then(data => console.log(new Date(data[0].created_at)))
             .catch(error => console.error(error));
     }
 
     getLatestCommit();
+
+
+    function wait(number) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (number === 1000) {
+                    resolve(`This message displays after ${number / 1000} second`);
+                }
+                if (number > 1000) {
+                    resolve(`This message displays after ${number / 1000} seconds`)
+                }
+                reject("This did not work");
+            }, number);
+        });
+    }
+
+    wait(1000).then((resolveMessage) => console.log(resolveMessage));
 
 }());
