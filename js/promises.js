3dd93e714e84a51d22f7382f78094276ce72b4e4
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-01-30 12:23:47 -0600

    working promises exercise

--- js/promises.js
+++ js/promises.js
@@ -5,8 +5,8 @@
     function getLatestCommit() {
         return fetch('https://api.github.com/users/stalbot0/events', {headers: {'Authorization': 'GITHUB_API_KEY2'}})
             .then(response => response.json())
-            .then(data => console.log(data[0]))
-            .catch(error => console.error(error))
+            .then(data => console.log(new Date(data[0].created_at)))
+            .catch(error => console.error(error));
     }
 
     getLatestCommit();
